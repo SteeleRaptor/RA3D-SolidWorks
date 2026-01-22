d629eca4bc067ce03be2f6729e21eacff1dfced4
D4C3D <jdpederson23@gmail.com>
2026-01-21 19:34:13 -0700

    Update Reach.m
    This code added calculates the forward kinematics (Given the joint angles the position(XYZ), yaw, pitch, and roll).

--- Reach.m
+++ Reach.m
@@ -16,12 +16,12 @@ clc;clear;close all;
 %These values are the inputs for the foward kinematics and include all
 %joint angles.
 % Should be in radians
-J1 = 0; %Range is -170 to 170 degrees
-J2 = 0; %Range is -42 to 90 degrees
-J3 = 0; % Range is -89 to 52 degrees
-J4 = 0; % Range is -165 to 165 degrees
-J5 = 0;% Range is -105 to 105 degrees
-J6 = 0;% Range is -155 to 155 degrees
+J1 = deg2rad(45); %Range is -170 to 170 degrees
+J2 = deg2rad(45); %Range is -42 to 90 degrees
+J3 = deg2rad(25); % Range is -89 to 52 degrees
+J4 = deg2rad(100); % Range is -165 to 165 degrees
+J5 = deg2rad(50);% Range is -105 to 105 degrees
+J6 = deg2rad(55);% Range is -155 to 155 degrees
 
 %% D-H parameters Table
 theta = [J1 (J2 - (pi/2)) (J3 + pi) J4 J5 J6];%Theta for MK3
@@ -37,14 +37,14 @@ T3 = Transformation(theta(3), d(3), a(3), alpha(3));%joint 2 to joint 3
 T4 = Transformation(theta(4), d(4), a(4), alpha(4));%joint 3 to joint 4
 T5 = Transformation(theta(5), d(5), a(5), alpha(5));%joint 4 to joint 5
 T6 = Transformation(theta(6), d(6), a(6), alpha(6));%joint 5 to joint 6
-Tfinal = T1*T2*T3*T4*T5*T6% Final transfermation matrix.
+Tfinal = (((((T1*T2)*T3)*T4)*T5)*T6)% Final transfermation matrix.
 %% Calculate yaw pitch and roll
-P = CalPitch(Tfinal);%y- axis
-R = CalRoll(P,Tfinal);%x-axis
-Y = CalYaw(P,Tfinal);%Theta
-P = rad2deg(P)
-R = rad2deg(R)
-Y = rad2deg(Y)
+Rz = CalYAW(Tfinal);
+Ry = CalPitch(Tfinal);
+Rx = CalRoll(Tfinal);
+Rz = rad2deg(Rz)
+Ry = rad2deg(Ry)
+Rx = rad2deg(Rx)
 %% Functions
 % Transfermation function
 function [T] = Transformation(theta, d, a, alpha)
@@ -54,19 +54,16 @@ function [T] = Transformation(theta, d, a, alpha)
          0 0 0 1];
 end
 %Yaw, pitch, and roll function
-%Calculates pitch(y)
-function [P] = CalPitch(T)
-    %P = atan2(-T(3,1),sqrt((T(1,1)^2)+(T(2,1)^2)));
-    P = atan2(T(1,3),T(2,3));
+%Calculates phi, rotation about the z-axis of joint 6(moving frame)
+function [Rz] = CalYAW(T)
+    Rz = atan2(T(2,1),T(1,1));
 end
-%Calculates roll(x)
-function [R] = CalRoll(P,T)
-    R = atan2(T(3,2)/cos(P),T(3,3)/cos(P));
-    %R = atan2(T(3,3)/cos(P),T(3,2)/cos(P));
-    %R = atan2(-T(1,1)*sin(P)+T(2,1)*cos(P), (-T(1,2)*sin(P)+T(2,2)*cos(P)));
+%Calculates theta, rotation about the o-axis(Think y-axis) of joint 6(moving
+%frame)
+function [Ry] = CalPitch(T)
+    Ry = atan2(-T(3,1),sqrt(T(1,1)^2 + T(2,1)^2));
 end
-%calculates yaw(z)
-function [Y] = CalYaw (P,T)
-    Y = atan2((T(1,3)*cos(P)+T(2,3)*sin(P)),T(3,3));
-    Y = atan2(T(2,1)/cos(P),T(1,1)/cos(P));
+%calculates psi, rotation about the 
+function [Rx] = CalRoll(T)
+    Rx = atan2(T(3,2),T(3,3));
 end
