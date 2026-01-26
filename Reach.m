ff0adcf888a095e89db4ff05d6b25cc3f5a5c594
D4C3D <jdpederson23@gmail.com>
2026-01-26 11:19:48 -0700

    Update Reach.m
    This code graphs chooses random joint angles and plots them on the 3d plot. This shows the general reach/workspace of the robotic arm. It also shows the points on a x-y graph, x-z graph, and y-z graph as well. It takes a bit to run, so I going to work on shows the arm in the graph and to optimize it so it doesn't take as long to run.

--- Reach.m
+++ Reach.m
@@ -4,91 +4,57 @@ clc;clear;close all;
 %Forward Kinematics for xyz
 %3D Scatter plot
 
-%% Purpose Statement
-% The purpose of this code is to calculate the forward kinematics of AR4
-% MK3 robotic arm. This will use the Denavit Hartenberg Method to determine
-% the transfer frunction of each joints. Since this is foward Kinematics
-% the input will be the joint angles for each arm. The output will be
-% position(x,y,z), yaw (rotation about z-axis), pitch (rotation about
-% y-axis), and roll (rotation about the x-axis).
-%% Constants
-J1min = -170;
-J1max = 170;
-J2min = -42;
-J2max = 90;
-J3min = -89;
-J3max = 52;
-J4min = -165;
-J4max = 165;
-J5min = -105;
-J5max = 105;
-J6min = -155;
-J6max = 155;
-%% main script
-for i = 1
-    J1 = rand(1)*(J1max-J1min)+J1min
-    J2 = rand(1)*(J2max-J2min)+J2min
-    J3 = rand(1)*(J3max-J3min)+J3min
-    J4 = rand(1)*(J4max-J4min)+J4min
-    J5 = rand(1)*(J5max-J5min)+J5min
-    J6 = rand(1)*(J6max-J6min)+J6min
-    forwardKinematics([J1,J2,J3,J4,J5,J6])
-end
 
-function forwardKinematics(JointAnglesDeg)
-    %% Set Joint angles
-    %These values are the inputs for the foward kinematics and include all
-    %joint angles.
-    % Should be in radians
-    J1 = deg2rad(45); %Range is -170 to 170 degrees
-    J2 = deg2rad(45); %Range is -42 to 90 degrees
-    J3 = deg2rad(25); % Range is -89 to 52 degrees
-    J4 = deg2rad(100); % Range is -165 to 165 degrees
-    J5 = deg2rad(50);% Range is -105 to 105 degrees
-    J6 = deg2rad(55);% Range is -155 to 155 degrees
-    
-    %% D-H parameters Table
-    theta = [J1 (J2 - (pi/2)) (J3 + pi) J4 J5 J6];%Theta for MK3
-    alpha = [(-pi/2) 0 (pi/2) (-pi/2) (pi/2) 0];%Alpha for MK3
-    d = [169.77 0 0 222.63 0 41];%D distance of MK3 
-    a = [64.2 305 0 0 0 0];%a distance of MK3
-    
-    %% Determine the transfermation functions
-    %This is used to calculate the transfermation matrix for each joint.
-    T1 = Transformation(theta(1), d(1), a(1), alpha(1));% Base to joint 1
-    T2 = Transformation(theta(2), d(2), a(2), alpha(2));% joint 1 to joint 2
-    T3 = Transformation(theta(3), d(3), a(3), alpha(3));%joint 2 to joint 3
-    T4 = Transformation(theta(4), d(4), a(4), alpha(4));%joint 3 to joint 4
-    T5 = Transformation(theta(5), d(5), a(5), alpha(5));%joint 4 to joint 5
-    T6 = Transformation(theta(6), d(6), a(6), alpha(6));%joint 5 to joint 6
-    Tfinal = (((((T1*T2)*T3)*T4)*T5)*T6)% Final transfermation matrix.
-    %% Calculate yaw pitch and roll
-    Rz = CalYAW(Tfinal);
-    Ry = CalPitch(Tfinal);
-    Rx = CalRoll(Tfinal);
-    Rz = rad2deg(Rz)
-    Ry = rad2deg(Ry)
-    Rx = rad2deg(Rx)
+% The purpose of this code is determine the workspace of the arm. To do
+% this random anlges will be selected from the angle range of each joint.
+% The position at these random angles will then be plotted on a 3D graph.
+% This will give a visualization of the range of the robotic arm.
+
+%% Set the Joint angles ranges
+for i = 1:1000
+J1 = deg2rad(randi([-170, 170])); %Range is -170 to 170 degrees
+J2 = deg2rad(randi([-42, 90]));  %Range is -42 to 90 degrees
+J3 = deg2rad(randi([-89,52])); %Range is -89 to 52 degrees
+J4 = deg2rad(randi([-165,165])); %Range is -165 to 165 degrees
+J5 = deg2rad(randi([-105,105])); %Range is -105 to 105 degrees
+J6 = deg2rad(randi([-155,155])); %Range is -155 to 155 degrees
+% D-H parameters Table
+theta = [J1 (J2 - (pi/2)) (J3 + pi) J4 J5 J6];%Theta for MK3
+alpha = [(-pi/2) 0 (pi/2) (-pi/2) (pi/2) 0];%Alpha for MK3
+d = [169.77 0 0 222.63 0 41];%D distance of MK3 
+a = [64.2 305 0 0 0 0];%a distance of MK3
+%Calculate the 
+T1 = Transformation(theta(1), d(1), a(1), alpha(1));% Base to joint 1
+T2 = Transformation(theta(2), d(2), a(2), alpha(2));% joint 1 to joint 2
+T3 = Transformation(theta(3), d(3), a(3), alpha(3));%joint 2 to joint 3
+T4 = Transformation(theta(4), d(4), a(4), alpha(4));%joint 3 to joint 4
+T5 = Transformation(theta(5), d(5), a(5), alpha(5));%joint 4 to joint 5
+T6 = Transformation(theta(6), d(6), a(6), alpha(6));%joint 5 to joint 6
+Tfinal = (((((T1*T2)*T3)*T4)*T5)*T6);% Final transfermation matrix.
+figure(1)
+plot3(Tfinal(1,4),Tfinal(2,4),Tfinal(3,4),"o");
+hold on
+figure(2)
+plot(Tfinal(1,4),Tfinal(2,4),"o") %x-axis vs y-axis
+xlabel("X-axis")
+ylabel("y-label")
+hold on
+figure(3)
+plot(Tfinal(1,4),Tfinal(3,4),"o")% x-axis vs z-axis
+xlabel("x-label")
+ylabel("y-label")
+hold on
+figure(4)
+plot(Tfinal(2,4),Tfinal(3,4),"o")%Y-axis vs z-axis
+xlabel("y-axis")
+ylabel("z-axis")
+hold on
 end
 %% Functions
-% Transfermation function
+% Transformation function
 function [T] = Transformation(theta, d, a, alpha)
-    T = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*cos(alpha) a*cos(theta);
+    T = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta);
          sin(theta) cos(theta)*cos(alpha) -cos(theta)*sin(alpha) a * sin(theta);
          0 sin(alpha) cos(alpha) d;
          0 0 0 1];
-end
-%Yaw, pitch, and roll function
-%Calculates phi, rotation about the z-axis of joint 6(moving frame)
-function [Rz] = CalYAW(T)
-    Rz = atan2(T(2,1),T(1,1));
-end
-%Calculates theta, rotation about the o-axis(Think y-axis) of joint 6(moving
-%frame)
-function [Ry] = CalPitch(T)
-    Ry = atan2(-T(3,1),sqrt(T(1,1)^2 + T(2,1)^2));
-end
-%calculates psi, rotation about the 
-function [Rx] = CalRoll(T)
-    Rx = atan2(T(3,2),T(3,3));
 end
